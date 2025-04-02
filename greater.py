64a2f2bf0d496185ddc48e0dae489e843f274267
VivasvanVerma <vvofficial.yt@gmail.com>
2025-04-02 20:20:17 +0530

    greater

---
+++ greater.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+if num>15:
+    print("{0} is greater than 15.".format(num))
+elif num<15:
+    print("{0} is smaller than 15.".format(num))
+else:
+    print("{0} is equal to 15.".format(num))
